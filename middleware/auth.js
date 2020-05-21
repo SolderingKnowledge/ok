2ebbf72ada4a9e97705ecc731c64e442ae03157c
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-05-21 01:47:31 -0700

    Authorization comment

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next){
+    // const token = req.header("Authorization"); => most people name it "Authorization"
     const token = req.header("x-auth-token");
     if(!token) return res.status(401).json({ message: "Authorization failed"});
 
