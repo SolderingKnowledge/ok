ea4a8666c82dbed048ebf17bb21d715639e9a3c9
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-05-22 23:22:38 -0700

    Profile CRUD setup almost done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -113,13 +113,58 @@ router.delete("/", auth, async (req, res)=> {
     try{
         await Profile.findOneAndRemove({user: req.user.id});
         await User.findByIdAndRemove({ _id: req.user.id });
-        
+
         res.json({message: "User deleted succesfully!"});
     }catch(error){
         console.error("error in catch", errors.message);
         res.status(500).send("Server failed");
     }
 })
+
+// route:           PUT api/profile/experience; like UPDATE
+// description:     Add profile experience
+// access:          Private
+router.put("/experience", auth, [
+    check("title", "Title is required").not().isEmpty(),
+    check("company", "Company is required").not().isEmpty(),
+    check("from", "From date is required").not().isEmpty(),
+], async (req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const { title, company, location, from, to, current, description } = req.body;
+    const newExperience = { title, company, location, from, to, current, description };
+    try{
+        const profile = await Profile.findOne({ user: req.user.id });// i will get from token
+        profile.experience.unshift(newExperience);
+        await profile.save();
+        res.json(profile);
+    }catch(error){
+        console.log("Error in catch", error.message);
+        res.status(500).send("Server failed");
+    }
+
+});
+
+// route:           DELETE  api/profile/experience/:exp_id
+// description:     delete experience from profile
+// access:          Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        // Get the remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.log("Error in catch", error.message);
+        res.status(500).send("Server failed");
+    }
+})
+
 
 
 
