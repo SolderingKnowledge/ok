16ce1295dc88f529733bf02a5a623cbe35189360
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-05-23 00:31:43 -0700

    setup github API and made request which returns list repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const config = require("config");
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
@@ -165,8 +167,74 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     }
 })
 
+// route:           PUT api/profile/education; like UPDATE
+// description:     Add profile education
+// access:          Private
+router.put("/education", auth, [
+    check("school", "School is required").not().isEmpty(),
+    check("degree", "Degree is required").not().isEmpty(),
+    check("fieldofstudy", "Field of study is required").not().isEmpty(),
+    check("from", "From date is required").not().isEmpty(),
+], async (req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+    const newEducation = { school, degree, fieldofstudy, from, to, current, description };
+    try{
+        const profile = await Profile.findOne({ user: req.user.id });// i will get from token
+        profile.education.unshift(newEducation);
+        await profile.save();
+        res.json(profile);
+    }catch(error){
+        console.log("Error in catch", error.message);
+        res.status(500).send("Server failed");
+    }
 
+});
 
+// route:           DELETE  api/profile/education/:edu_id
+// description:     delete education from profile
+// access:          Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        // Get the remove index
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+        profile.education.splice(removeIndex, 1);
+
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.log("Error in catch", error.message);
+        res.status(500).send("Server failed");
+    }
+})
+// route:           GET  api/profile/github/:username
+// description:     get user repos from github
+// access:          Public
+router.get("/github/:username", async (req, res)=> {
+    try {
+        const options = { 
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubClientSecret")}`,
+            method: "GET",
+            headers: {"user-agent": "node.js"}
+        };
+        request(options, (error, response, body)=> {
+            if(error) console.error(error);
+            if(response.statusCode !== 200){
+                return res.status(404).json({message: "Github account not found!"})
+            }
+            res.json(JSON.parse(body));
+        });
+    } catch (error) {
+        console.error("error in catch", error.message);
+        console.error(error.message);
+        res.status(500).send("Server failed");
+
+    }
+})
 
 
 // router.get("/", (req, res) => {
